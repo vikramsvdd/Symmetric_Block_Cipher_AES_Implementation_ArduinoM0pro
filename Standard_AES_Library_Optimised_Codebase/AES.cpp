2a79aab5f8d768cc5f1fb39ea49e9162454ae30e
vikramsvdd <51330665+vikramsvdd@users.noreply.github.com>
2022-11-25 22:00:47 +0530

    Update AES.cpp

--- Standard_AES_Library_Optimised_Codebase/AES.cpp
+++ Standard_AES_Library_Optimised_Codebase/AES.cpp
@@ -1,4 +1,8 @@
 #include "AES.h"
+/* **NOTE: This code is part of the standard AES Library. This code has been modified in the following aspects
+1. References to pgmspace has been removed as it is unnecessary for Arduino M0 pro (32-bit cortex ARM-M0+) or any ARM based environment
+2. References to program memory or flash has been removed and thus SRAM is the only memory being used, this has been done due to the intrinsic advantage that Arduino M0 pro has : 32kb RAM which is huge by AVR standards, this library has been originally designed for AVR systems and thus we modified these aspects and tried to use the resources offered by arduino M0 pro to the fullest.
+Also, it is important to note that the Encryption and Decryption function implementation is un-altered, thus it is absolutely safe to use this library and it can be called the official version due to the core functionality being un-changed, though technically the library has been modified to better utilize the resources offered by Arduino M0 pro** */
 
 /*
  ---------------------------------------------------------------------------
